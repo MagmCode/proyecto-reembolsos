39049dd15bb05bd7ecd427216e96fcaf27889cdc
MagmCode <mariaaguedez.08@gmail.com>
2025-02-06 18:03:04 -0400

    #Update
    Vista de reembolso

--- src/app/modules/user/reembolso/reembolso.component.ts
+++ src/app/modules/user/reembolso/reembolso.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface Factura {
+  nroFactura: string;
+  nroControl: string;
+  fechaFactura: string;
+  concepto: string;
+  monto: string;
+  comprobante: string;
+}
+
+const ELEMENT_DATA: Factura[] = [
+  {nroFactura: '', nroControl: '', fechaFactura: '', concepto: '', monto: '', comprobante: ''},
+  // Add more rows as needed
+];
 
 @Component({
   selector: 'app-reembolso',
   templateUrl: './reembolso.component.html',
   styleUrls: ['./reembolso.component.scss']
 })
-export class ReembolsoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ReembolsoComponent {
+  displayedColumns: string[] = ['nroFactura', 'nroControl', 'fechaFactura', 'concepto', 'monto', 'comprobante', 'actions'];
+  dataSource = ELEMENT_DATA;
 }

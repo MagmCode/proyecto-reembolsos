fce458bc8dd708a67b5ea5a78c1554921f360abd
MagmCode <mariaaguedez.08@gmail.com>
2025-02-19 15:45:56 -0400

    #update
    se modifico el boton

--- src/app/modules/pages/unauthorized/unauthorized.component.ts
+++ src/app/modules/pages/unauthorized/unauthorized.component.ts
@@ -11,12 +11,7 @@ export class UnauthorizedComponent {
 
   constructor(private location: Location, private router: Router) {}
 
-  goBack(): void {
-    if (window.history.length > 1) {
-      this.location.back();
-    } else {
-      // Redirigir a una ruta predeterminada si no hay historial
-      this.router.navigate(['/']);
-    }
+  salir(): void {
+      this.router.navigate(['/Login']);
   }
 }

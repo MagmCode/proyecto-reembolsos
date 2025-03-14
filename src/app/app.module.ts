51fc2a80530ecabdcad42f10b5d97fa35b08271e
MagmCode <mariaaguedez.08@gmail.com>
2025-03-14 00:01:32 -0400

    Remove MatListModule import from app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,7 +38,7 @@ import { MatCardModule } from '@angular/material/card';
 import {MatStepperModule} from '@angular/material/stepper';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { CsrfInterceptor } from './services/csrf.interceptor';
-import { MatListModule } from '@angular/material/list';
+// import { MatListModule } from '@angular/material/list';
 
 
 
@@ -145,7 +145,7 @@ import { ReportesAnalistComponent } from './modules/rol/analist/reportes-analist
     MatSidenavModule,
     MatStepperModule,
     MatProgressSpinnerModule,
-    MatListModule
+    
     
     
 
